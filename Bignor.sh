2247719a34348b34c7b37d4140994e630c826599
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-01-11 14:47:43 +0530

    Create Bignor.sh

---
+++ Bignor.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+num1=$1
+num2=$2
+if [ $1 -gt $2 ]
+then 
+	echo " $1 is biggest"
+elif [ $1 -eq $2 ]
+then
+	echo " $1 & $2 are equal"
+else
+	echo " $2 is biggest"
+fi
+echo " Name of the file is $0"
+echo " ProcessID of current process is $$" &
+echo " Total number of arrguments passed : $#"
+echo " ProcessID of previous process is $!" 
