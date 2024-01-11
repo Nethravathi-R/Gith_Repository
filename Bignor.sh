119a3d5994cbd205d1f14f4b083f2ce93c655f80
NethravathiR <154945584+Nethravathi-R@users.noreply.github.com>
2024-01-11 15:52:23 +0530

    Update Bignor.sh

--- Bignor.sh
+++ Bignor.sh
@@ -5,7 +5,7 @@ if [[ $1 -gt $2 ]]
 then 
 	echo " $1 is biggest"
 fi
-echo " Name of the file is $0"
+
 echo " ProcessID of current process is $$" &
 echo " Total number of arrguments passed : $#"
 echo " ProcessID of previous process is $!" 
